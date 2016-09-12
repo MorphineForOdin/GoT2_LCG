cc7a847824b36e689ecf785ac2e6c2b2ca36be9d
Svyatoslav Malinovsky <svyatoslav.malinovskyj@gmail.com>
2016-09-12 21:26:44 +0300

    Add base enums to domain project task: https://github.com/MorphineForOdin/GoT2_LCG/issues/6

--- GotLcg/GotLcg.Domain/Enums/CardType.cs
+++ GotLcg/GotLcg.Domain/Enums/CardType.cs
@@ -1,7 +1,43 @@
 ﻿namespace GotLcg.Domain.Enums
 {
+    /// <summary>
+    /// Defines different card types.
+    /// </summary>
     public enum CardType
     {
-        // TODO: populate the emun after database bootstrap data
+        /// <summary>
+        /// Faction card (Starks, Lannister, etc.).
+        /// </summary>
+        Faction = 1,
+
+        /// <summary>
+        /// Agenda card, contains general strategy for the game.
+        /// </summary>
+        Agenda,
+
+        /// <summary>
+        /// Plot card that specify strategy for one round.
+        /// </summary>
+        Plot,
+
+        /// <summary>
+        /// Character card.
+        /// </summary>
+        Character,
+
+        /// <summary>
+        /// Location card.
+        /// </summary>
+        Location,
+
+        /// <summary>
+        /// Attachment card.
+        /// </summary>
+        Attachment,
+
+        /// <summary>
+        /// Event card.
+        /// </summary>
+        Event
     }
 }
--- GotLcg/GotLcg.Domain/Enums/ChallengeIcon.cs
+++ GotLcg/GotLcg.Domain/Enums/ChallengeIcon.cs
@@ -1,7 +1,23 @@
 ﻿namespace GotLcg.Domain.Enums
 {
+    /// <summary>
+    /// Defines different challenge icon types.
+    /// </summary>
     public enum ChallengeIcon
     {
-        // TODO: populate the emun after database bootstrap data
+        /// <summary>
+        /// Military challenge with the goal  to kill an opponent’s characters.
+        /// </summary>
+        Military = 1,
+
+        /// <summary>
+        /// Intrigue challenge with the goal to discard cards from an opponent’s hand.
+        /// </summary>
+        Intrigue,
+
+        /// <summary>
+        /// Power challenge with the goal to move power from an opponent’s faction card to yours.
+        /// </summary>
+        Power
     }
 }
